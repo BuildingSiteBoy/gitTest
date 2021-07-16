f95286686466ba1213d4cba1487b112f221751d3
zZeng <zZengx@tt.com>
2021-07-16 22:33:11 +0800

    second commit from zZ

--- src/main/java/com/zZeng/git/Test00Git.java
+++ src/main/java/com/zZeng/git/Test00Git.java
@@ -3,5 +3,6 @@ package com.zZeng.git;
 public class Test00Git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git22");
     }
 }
